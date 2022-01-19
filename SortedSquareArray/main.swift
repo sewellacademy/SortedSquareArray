f760d809eb52c1ba07244df021ccfca56903c64a
dylanswl <dvsewell15@gmail.com>
2022-01-19 08:37:20 -0500

    sorted squares problem

--- SortedSquareArray/main.swift
+++ SortedSquareArray/main.swift
@@ -7,5 +7,42 @@
 
 import Foundation
 
-print("Hello, World!")
+class Program {
+    // O(nlogn) time | O(n) space
+    static func sortedSquareArray(_ array: [Int]) -> [Int] {
+        var sortedSquares = Array(repeating: 0, count: array.count)
+        
+        for idx in stride(from: 0, to: array.count, by: 1) {
+            let value = array[idx]
+            sortedSquares[idx] = value * value
+        }
+        sortedSquares.sort()
+        
+        return sortedSquares
+    }
+    
+    static func sortedSquareArray2(_ array: [Int]) -> [Int] {
+        var sortedSquares = Array(repeating: 0, count: array.count)
+        
+        var smallerValueIdx = 0
+        var largerValueIdx = array.count - 1
+        
+        for idx in stride(from: array.count - 1, through: 0, by: -1) {
+            let smallerValue = array[smallerValueIdx]
+            let largerValue = array[largerValueIdx]
+            
+            if abs(smallerValue) > abs(largerValue) {
+                sortedSquares[idx] = smallerValue * smallerValue
+                smallerValueIdx += 1
+            } else {
+                sortedSquares[idx] = largerValue * largerValue
+                largerValueIdx -= 1
+            }
+        }
+        return sortedSquares
+    }
+}
 
+let arr = [1, 2, 3, 5, 6, 8, 9]
+print(Program.sortedSquareArray(arr))
+print(Program.sortedSquareArray2(arr))
